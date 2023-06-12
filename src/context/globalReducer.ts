75bd5192aa4958142f8057436dcad62f7937692a
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-12 19:05:10 -0300

    feat: Se agrego un nuevo type ADD_TRANSACTIONS que controla todas las transacciones

--- src/context/globalReducer.ts
+++ src/context/globalReducer.ts
@@ -1,15 +1,24 @@
 import { GlobalActions, GlobalState } from "../interfaces";
 
 export const INITIAL_STATE: GlobalState = {
-  name: '',
+  name: 'Leandro',
+  transactions: []
 };
 
-export const globalReducer = (_state: GlobalState, action: GlobalActions) => { 
+export const globalReducer = (state: GlobalState, action: GlobalActions): GlobalState => { 
 
   switch (action.type) {
     case 'setNameUser':
       return {
+        ...state,
         name: action.payload
       }
+    case 'ADD_TRANSACTIONS':
+      return {
+        ...state,
+        transactions: action.payload
+      }
+    default: 
+      return state
   }
 };
