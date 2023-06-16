dcc84f6cac8ab514de22f00f5f0b0bd0530067a2
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-16 20:59:52 -0300

    feat: Se agrego nueva funcionabilidad, ir al localStorage para setear el state actual

--- src/context/GlobalProvaider.tsx
+++ src/context/GlobalProvaider.tsx
@@ -1,23 +1,27 @@
-import { ChangeEvent, useReducer, useState } from "react";
+import { ChangeEvent, useEffect, useReducer, useState } from "react";
 import { v4 as uuidv4} from 'uuid'
 
 import { GlobalContext } from "./GlobalContext"
 import { GlobalProps } from "../interfaces";
-import { globalReducer } from ".";
-import { INITIAL_STATE } from "./globalReducer";
+import { INITIAL_STATE, globalReducer } from "./globalReducer";
 import { Transactions } from '../interfaces/index';
 
 let data: Transactions;
 
-export const GlobalProvaider = ({ children }: GlobalProps): JSX.Element => {
+const GlobalProvaider = ({ children }: GlobalProps): JSX.Element => {
   const [state, dispatch] = useReducer(globalReducer, INITIAL_STATE);
   const [selectOthers, setSelectOthers] = useState<boolean>(false);
-  const [selectedValue, setSelectedValue] = useState<string>('');
+  const [selectedValue, setSelectedValue] = useState<string>("");
 
-  // Cambia el estado del selectOthers 
+  // Seteamos el localStorge
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(state.transactions));
+  }, [state.transactions]);
+
+  // Cambia el estado del selectOthers
   const handleToggle = (e: ChangeEvent<HTMLSelectElement>) => {
     e.target.value === "Otros" ? setSelectOthers(true) : setSelectOthers(false);
-    setSelectedValue(e.target.value)
+    setSelectedValue(e.target.value);
   };
 
   // Crea una nueva transaccion
@@ -27,11 +31,11 @@ export const GlobalProvaider = ({ children }: GlobalProps): JSX.Element => {
       description,
       mount,
       options: selectedValue,
-    }
-    
+    };
+
     dispatch({ type: "ADD_TRANSACTIONS", payload: data });
-  }
-  
+  };
+
   return (
     <GlobalContext.Provider
       value={{
@@ -48,3 +52,5 @@ export const GlobalProvaider = ({ children }: GlobalProps): JSX.Element => {
     </GlobalContext.Provider>
   );
 };
+
+export default GlobalProvaider;
