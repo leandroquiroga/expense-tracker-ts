cd2212443a0c18771ca658f203fb3ec8de97ae50
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-16 20:54:47 -0300

    feat: Nuevos componentes

---
+++ src/components/TransactionList/TransactionList.tsx
@@ -0,0 +1,12 @@
+// import { useGlobalState } from "../../hooks/useGlobalState";
+
+const TransactionList = () => {
+  
+  return (
+    <>
+      TransactionList
+    </>
+  )
+};
+
+export default TransactionList;
