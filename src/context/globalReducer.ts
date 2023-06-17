0ec9aab7e389eb4362db3576de9cb7468470e765
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-16 21:00:33 -0300

    fix: Se modifico los valores del INITIA_STATE

--- src/context/globalReducer.ts
+++ src/context/globalReducer.ts
@@ -1,27 +1,24 @@
 import { GlobalActions, GlobalState } from "../interfaces";
 
 export const INITIAL_STATE: GlobalState = {
-  name: 'Leandro',
-  transactions: []
+  name: "Leandro", // Con el nombre realizar lo mismo, ya que va consumir el nombre del login que se guardara en localStorage ya que no hay bases de datos
+  transactions: localStorage.getItem("transactions") ? JSON.parse(localStorage.getItem("transactions") || "") : [],
 };
 
 export const globalReducer = (state: GlobalState, action: GlobalActions): GlobalState => { 
 
   switch (action.type) {
-    case 'setNameUser':
+    case "setNameUser":
       return {
         ...state,
-        name: action.payload
-      }
-    case 'ADD_TRANSACTIONS':
+        name: action.payload,
+      };
+    case "ADD_TRANSACTIONS":
       return {
         ...state,
-        transactions: [
-          ...state.transactions,
-          action.payload
-        ]
-      }
-    default: 
-      return state
+        transactions: [...state.transactions, action.payload],
+      };
+    default:
+      return state;
   }
 };
