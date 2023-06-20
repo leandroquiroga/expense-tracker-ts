28807d47f52059d9879ca535743d84d222dffbc3
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-20 18:54:28 -0300

    feat: Se agrego la funcionalidad de volver al home en el logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import { Outlet } from "react-router-dom";
-import { useGlobalState} from "../../hooks/useGlobalState.ts";
+import { Outlet, Link } from "react-router-dom";
+import { useGlobalState } from "../../hooks/useGlobalState.ts";
 import { container, container_img, container_text_name } from "./header.css.ts";
+
 import logoHeader from '/icon-app.svg';
 
 export const Header = () => {
@@ -9,7 +10,9 @@ export const Header = () => {
   return (
     <>
       <nav className={`${container}`}>
-        <img src={logoHeader} alt="logo" className={`${container_img}`} />
+        <Link to="/">
+           <img src={logoHeader} alt="logo" className={`${container_img}`} />
+        </Link>
         <section>
           <p className={`${container_text_name}`}>Hola! {name}</p>
         </section>
