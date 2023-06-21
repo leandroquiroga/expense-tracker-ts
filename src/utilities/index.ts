210d0c87f96fc3a2b18c01f986a5151a042c1c55
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-21 18:44:48 -0300

    feat: Se agrego metodos que calcular la diferencia horaria, si el monto es negativo y el formateador de fechas

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { DATE_UNITS_Interfaces, GetUnitAndValueDate } from '../interfaces';
 
 export const arrayOptions = [
   "Seleccione una opcion",
@@ -28,3 +29,40 @@ export const validationForm = {
     .required('Por favor escoje un opcion')
     .not(["Seleccione una opcion"], 'Escoja una opcion valida'),
 };
+
+export const amountNegative = (amount: number): boolean => (amount < 0) ? true : false;
+
+// Configuracion del tiempo desde que se actualizo el estado
+
+export const DATE_UNITS: DATE_UNITS_Interfaces = {
+  day: 86400,
+  hour: 3600,
+  minute: 60,
+  second: 1,
+};
+
+// Calcula el numero de segundos que a pasado desde el timpo actualizado al momento actual
+export const getSecondsDiff = (timestaps: number): number => (Date.now() - timestaps) / 1000;
+
+
+// Dependiendo el numero de segundo calcula los dias, semanas, horas, segundos que a pasado
+// donde la unidad por ejemplo si fueron 5 dias que han pasado devuelve en unit: dias y en el value: -5
+export const getUnitAndValueDate = (
+  secondsElapsed: number
+): GetUnitAndValueDate | void => {
+  for (const [unit, secondsInUnit] of Object.entries(DATE_UNITS)) {
+    if (secondsElapsed >= secondsInUnit || unit === "second") {
+      const value = Math.floor(secondsElapsed / secondsInUnit) * -1;
+      return { value, unit };
+    }
+  }
+};
+
+// Se encarga de formatear la fecha por ejemplo "Hace 5 dias"
+export const getTimeAgo = (timestamp: any, locale: string): string => {
+  const relativeTimeFormat = new Intl.RelativeTimeFormat(locale);
+
+  const secondsElapsed = getSecondsDiff(timestamp);
+  const {value, unit} = getUnitAndValueDate(secondsElapsed);
+  return relativeTimeFormat.format(value, unit);
+}
