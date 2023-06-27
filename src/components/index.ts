c624d43cbb5c7c305e77ab1c15b1b0f6a8cbb7c0
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-27 18:04:37 -0300

    fix(Cambio): Se cambiaron todos los metodos de exportacion

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,9 @@
-export { default as PageNotFound} from "./PageNotFound/PageNotFound";
-export { default as TransactionList} from "./TransactionList/TransactionList";
+export { default as Balance} from './TrasanctionBalance/Balance';
+export { default as Dasboard } from "./Dashboard/Dasboard";
 export { default as FormComponent } from "./Form/Form";
+export { default as LinkButton} from "./LinkButton/LinkButton";
+export { default as PageNotBalance} from './PageNotBalance/PageNotBalance';
+export { default as PageNotFound} from "./PageNotFound/PageNotFound";
+export { default as Title} from './Title/Title';
+export { default as TransactionList} from "./TransactionList/TransactionList";
 export { Header } from "./Header/Header";
