b65c993d57300cc720658e474bafe3d23004d5b6
leandroquiroga <leandroquiroga9514@gmail.com>
2023-06-30 17:17:09 -0300

    feat:(type) Se creo la logica para almacenar el nombre del usuario en el localstorage

--- src/context/globalReducer.ts
+++ src/context/globalReducer.ts
@@ -1,18 +1,17 @@
 import { GlobalActions, GlobalState } from "../interfaces";
 
 export const INITIAL_STATE: GlobalState = {
-  name: "Leandro", // Con el nombre realizar lo mismo, ya que va consumir el nombre del login que se guardara en localStorage ya que no hay bases de datos
-  transactions: localStorage.getItem("transactions") ? JSON.parse(localStorage.getItem("transactions") || "") : [],
+  name: localStorage.getItem("name")
+    ? JSON.parse(localStorage.getItem("name") || "")
+    : "",
+  transactions: localStorage.getItem("transactions")
+    ? JSON.parse(localStorage.getItem("transactions") || "")
+    : [],
 };
 
 export const globalReducer = (state: GlobalState, action: GlobalActions): GlobalState => { 
 
   switch (action.type) {
-    case "setNameUser":
-      return {
-        ...state,
-        name: action.payload,
-      };
     case "ADD_TRANSACTIONS":
       return {
         ...state,
@@ -23,6 +22,12 @@ export const globalReducer = (state: GlobalState, action: GlobalActions): Global
       return {
         ...state,
         transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+      }
+    
+    case "LOGIN": 
+      return {
+        ...state,
+        name: localStorage.setItem("name", JSON.stringify(action.payload))!
       }
     default:
       return state;
