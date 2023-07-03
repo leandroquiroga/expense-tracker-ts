a332dfdcebc221e6a8cb5d108af4b8a9a1e81973
leandroquiroga <leandroquiroga9514@gmail.com>
2023-07-03 15:42:46 -0300

    fix: (Path) Se cambio el path de importacion de la ruta del router

--- src/ExpenseTracker.tsx
+++ src/ExpenseTracker.tsx
@@ -1,12 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { GlobalProvaider } from './context';
-import router from './router/index.router';
+import route from './router/index.router';
 
 export const ExpenseTracker = () => {
   return (
     <GlobalProvaider>
-      <RouterProvider router={router} fallbackElement={<p>Cargando ...</p>} />
+      <RouterProvider router={route} fallbackElement={<p>Cargando ...</p>} />
     </GlobalProvaider>
   );
 }
