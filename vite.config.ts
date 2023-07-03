35b0f601bee296df7976f73b249ce2a0478deb87
leandroquiroga <leandroquiroga9514@gmail.com>
2023-07-03 15:44:08 -0300

    feat: (New) Se agrego un nuevo plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
+import reactRefresh from "@vitejs/plugin-react-refresh";
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vanillaExtractPlugin()
-  ],
+  plugins: [react(), reactRefresh(), vanillaExtractPlugin()],
 });
