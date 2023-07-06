3799ab2d54b89e90ef032254e777418ba1fcb9cc
leandroquiroga <leandroquiroga9514@gmail.com>
2023-07-06 20:06:51 -0300

    fix: (bugs) Se soluciono el bug de los argumentos

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,39 +1,31 @@
 import { useState } from "react"
+import { LocalStorageHook, LocalStorageValue } from "../interfaces";
 
-// Valores que almacena nuestro localStorage
-type LocalStorageValue = string | null
-
-//Iterfaces
-interface LocalStorageHook {
-  getItemLocalStorage: () => LocalStorageValue
-  setItemLocalStorage: (value: LocalStorageValue) => void;
-}
-
-const useLocalStorage = (key: string): LocalStorageHook => {
-
+const useLocalStorage = (key?: string): LocalStorageHook => {
   const [localStorageValue, setLocalStgorageValue] = useState<LocalStorageValue>(() => {
+      // Verificamos si existe la key
+      if (!key) return null;
 
-    //Itentamos obtener el valor del localStorage
-    const storedValue = localStorage.getItem(key);
-    return storedValue !== null ? storedValue : null;
-  });
-
+      //Itentamos obtener el valor del localStorage
+      const storedValue = localStorage.getItem(key);
+      return storedValue !== null ? storedValue : null;
+    });
 
   //Obtenemos el valor del localStorage
   const getItemLocalStorage = (): LocalStorageValue => localStorageValue;
 
   // Seteamos un valor en el localStorage
-  const setItemLocalStorage = (value: LocalStorageValue): void => {
-
+  const setItemLocalStorage = ( transaction: string, value: LocalStorageValue): void => {
     // Actualizamos el estado.
     setLocalStgorageValue(value);
-    value !== null ? localStorage.setItem(key, value) : localStorage.removeItem(key);
+    value !== null
+      ? localStorage.setItem(transaction, value)
+      : localStorage.removeItem(transaction);
   };
-
 
   return {
     getItemLocalStorage,
-    setItemLocalStorage
+    setItemLocalStorage,
   };
 };
 
